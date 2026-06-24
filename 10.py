38475a95b6b669c1553771c3acad5d70a8363023
mounikamutipogu <mounikamuthipogu@gmail.com>
2026-06-24 10:30:18 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,8 @@
+n=int(input())
+for i in range(n):
+    print(" "*(n-i-1),end="")
+    for j in range(i+1):
+        print(chr(65+j),end="")
+    for j in range(i-1,-1,-1):
+        print(chr(65+j),end="")
+    print()    
